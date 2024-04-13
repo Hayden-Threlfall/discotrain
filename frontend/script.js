bc2190e2990161a5eb34cb492548cf3ab6023018
gharding04 <gh030322@gmail.com>
2024-04-13 04:31:04 -0500

    Trending Page
    Trending page. Needs Preview fixed, play added, nav centered.

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.carousel').carousel();
   });
+
+function toggleVideo() {
+    const preview = document.querySelector('.preview');
+    const video = document.querySelector('video');
+    video.pause();
+    preview.classList.toggle('active');
+}
